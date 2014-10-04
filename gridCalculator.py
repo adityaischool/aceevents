953f560421f586a29b46ac05c1de645f5609c155
Alexander Jones <alexander.jones@ischool.berkeley.edu>
2014-10-04 16:58:09 -0700

    comment

---
+++ gridCalculator.py
@@ -0,0 +1,90 @@
+#bigGridCorners are SF bounding box corner coords going from top-left then clockwise
+bigGridCorners = [[37.836698500000004, -122.515500], [37.836698500000004, -122.369145], [37.703206, -122.515500], [37.703206, -122.369145]]
+
+smallGridLats = []
+smallGridLongs = []
+
+latIncrement = 0
+longIncrement = 0
+
+smallGridCorners = []
+
+
+def main():
+	getBigGridMid(bigGridCorners)
+	calcIncrements(bigGridCorners)
+	calcSmallGridLatsLongs(bigGridCorners)
+	calcSmallGridCorners(smallGridLats, smallGridLongs)
+
+def getBigGridMid(corners):
+	
+
+	bigGridLatMid = (corners[1][0] + corners[2][0]) / 2
+	bigGridLongMid = (corners[0][1] + corners[1][1]) / 2
+
+	#print "bigGridLatMid = ", str(bigGridLatMid)
+	#print "bigGridLongMid = ", str(bigGridLongMid)
+
+def calcIncrements(corners):
+	global latIncrement, longIncrement
+
+	bigGridLatDistance = (corners[1][0] - corners[2][0])
+	bigGridLongDistance = (corners[0][1] - corners[1][1])
+
+	#print "bigGridLatDistance = ", str(bigGridLatDistance)
+	#print "bigGridLongDistance = ", str(bigGridLongDistance)
+
+	latIncrement = bigGridLatDistance / 4
+	longIncrement = bigGridLongDistance / 4
+
+	#print "latIncrement = ", str(latIncrement)
+	#print "longIncrement = ", str(longIncrement)
+
+def calcSmallGridLatsLongs(corners):
+	global smallGridLats, smallGridLongs
+
+	for i in range(5):
+
+		smallGridLats.append(bigGridCorners[0][0] - (i * latIncrement))
+		smallGridLongs.append(bigGridCorners[0][1] - (i * longIncrement))
+
+	#print "smallGridLats = ", smallGridLats
+	#print "smallGridLongs = ", smallGridLongs
+
+def calcSmallGridCorners(lats, longs):
+	global smallGridCorners
+
+	rowCounter = 0
+
+	latStart = bigGridCorners[0][0]
+	longStart = bigGridCorners[0][1]
+
+	#for i in range(16):
+	#	smallGridCorners.append([])
+
+	for i in range(16):
+		row = i / 4
+		points = [[0,0], [0,0], [0,0], [0,0]]
+
+		i2 = i % 4
+
+		points[0] = [latStart + row * latIncrement, longStart + i2 * longIncrement]
+		points[1] = [latStart + row * latIncrement, longStart + (i2 + 1) * longIncrement]
+		points[2] = [latStart + (row + 1) * latIncrement, longStart + i2 * longIncrement]
+		points[3] = [latStart + (row + 1) * latIncrement, longStart + (i2 + 1) * longIncrement]
+
+		#val1x=(bigGridCorners[0][0] - (row*latIncrement)) - (row*latIncrement)
+		#val1y=(bigGridCorners[0][1] - (row*longIncrement)) - (col*longIncrement)
+
+		smallGridCorners.append(points)
+
+	#for i in smallGridCorners:
+	#	print "Grid", str(i), ":",
+	#	for i2 in i:
+	#		print str(i[i2])
+	print "smallGridCorners = ", smallGridCorners
+
+
+	
+if __name__ == '__main__':
+	main()
